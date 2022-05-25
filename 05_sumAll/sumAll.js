4acecbdab5adb6855915fb1d2aa6ed37015d5296
Jay <djmroid@gmail.com>
2022-05-24 23:54:54 -0400

    finished exercise five, have yet to complete othern tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,14 @@
-const sumAll = function() {
-
-};
+const sumAll = function(min,max) {
+let sumNum = 0;
+if (typeof(min) === 'string' || typeof(max) === 'string'){
+    return 'ERROR';
+}else{
+for(let i = min; i<= max; i++){
+sumNum += i;
+}
+return sumNum;
+}
+}
 
 // Do not edit below this line
 module.exports = sumAll;

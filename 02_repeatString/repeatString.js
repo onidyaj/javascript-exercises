a298a4642372c37d750a2758c0589755f32b3611
Jay <djmroid@gmail.com>
2022-05-24 22:43:15 -0400

    finished exercise two

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
-};
+function repeatString(word, num){
+    let result = "";
+    if (num < 0){return 'ERROR'
+} else
+    for( let i = 0; i < num; i++){
+        result += word
+    }
+    return result;
+    }
 
 // Do not edit below this line
 module.exports = repeatString;
